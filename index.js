fb6a47083cc12b8e8b58e632b131ec816248acf2
Kevin Chaney <kevin_chaney@icloud.com>
2022-05-05 00:09:04 -0700

    commit tree 9a5ae2864357d43fd69c2783224e6547198dab68

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat() {
+    cats.push('Ralph'); 
+}
+function destructivelyPrependCat() {
+    cats.unshift('Bob');
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(cat) {
+    const newArray = [...cats, cat];
+    return newArray
+}
+function prependCat(cat) {
+    const newArray = [cat, ...cats];
+    return newArray
+}
+function removeLastCat() {
+    const newArray = [...cats]
+    newArray.pop();
+    return newArray;
+}
+function removeFirstCat() {
+    const newArray = [...cats]
+    newArray.shift();
+    return newArray;
+}
